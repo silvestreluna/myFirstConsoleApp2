8d6eb89d143efee08a9a5008400ecddffa13dc99
Silvestre Luna <slvtrluna@gmail.com>
2019-08-20 22:57:28 -0500

    Add project files.

---
+++ myFirstConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myFirstConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
