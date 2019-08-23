1b69edb59edd11819b00f8720d44e21e4de44773
Silvestre Luna <slvtrluna@gmail.com>
2019-08-22 21:57:36 -0500

    completed user input

---
+++ acceptingUserInput/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace acceptingUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo enteredKey;
+
+            do
+            {
+                enteredKey = Console.ReadKey();
+                Console.WriteLine($"You pressed the {enteredKey.Key.ToString()} key");
+
+            } while (enteredKey.Key != ConsoleKey.Escape);
+
+            // Console.Read();
+            Console.WriteLine("test");
+
+            // testing toChar();
+            int enetedItem;
+            enetedItem = Console.Read();
+            var convertedItem = Convert.ToChar(enetedItem);
+            Console.WriteLine($"{convertedItem}");
+            Console.WriteLine($"{enetedItem}, Withot Conversion");
+
+
+            string sentence;
+
+            do
+            {
+                Console.WriteLine("Type in a sentence. Press enter when done.");
+                sentence = Console.ReadLine();
+
+                Console.WriteLine($"You typed {sentence}");
+            } while (sentence != "quit");
+
+        }
+    }
+}
