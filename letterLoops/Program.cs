d2d5d425c441ac6a50b1ea66c61d6124e3da6477
Silvestre Luna <slvtrluna@gmail.com>
2019-08-26 17:55:37 -0500

    wip

---
+++ letterLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace letterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFromUser = Console.ReadLine().ToString();
+
+            foreach (var eachLetter in inputFromUser)
+            {
+                int indexOfEachLetter = inputFromUser.IndexOf(eachLetter);
+
+                string thisLetter = eachLetter.ToUpper();
+
+                Console.WriteLine(indexOfEachLetter);
+            }
+            
+            // Console.WriteLine($"{inputFromUser}, ...TEST");
+        }
+    }
+}
