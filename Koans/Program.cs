520aecec393aebd0067c0967a4319cfd9ad4c7ed
Silvestre Luna <slvtrluna@gmail.com>
2019-08-31 09:20:32 -0500

    remove space

---
+++ Koans/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Koans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
